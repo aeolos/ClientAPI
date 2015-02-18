8ad1fbbfde992929b239bf01d60ddfe5a12c6953
Jimmy Hennessy <jimmy@woden.ie>
2015-02-18 10:31:44 +0000

    Added Jersey MVC
    Add ability to server JSP pages via a filter

--- src/main/java/com/aeolos/controller/EmployeeController.java
+++ src/main/java/com/aeolos/controller/EmployeeController.java
@@ -1,35 +1,16 @@
 package com.aeolos.controller;
 
-import java.util.List;
-
 import javax.ws.rs.GET;  
 import javax.ws.rs.Path;  
-import javax.ws.rs.PathParam;  
 import javax.ws.rs.Produces;  
-import javax.ws.rs.core.MediaType;  
-import com.aeolos.dao.EmployeeRepo;
-import com.aeolos.model.Employee; 
-import com.aeolos.service.EmployeeService;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.glassfish.jersey.server.mvc.Viewable;
 
 @Path("/employee")  
 public class EmployeeController{  
 	
-	private EmployeeRepo repo = new EmployeeRepo();
-	private EmployeeService service = new EmployeeService(repo);
-		
-    @GET  
-    @Produces(MediaType.APPLICATION_JSON)  
-    @Path("{id}")  
-    public Employee read(@PathParam("id") long id) {   	
-    	return service.read(id);
-    }  
-    
-    @GET  
-    @Produces(MediaType.APPLICATION_JSON) 
-    public List<Employee> store() {  
-    	return service.store();
-    }
-    
-    
+ 
 }
     
